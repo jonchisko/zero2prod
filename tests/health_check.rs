4e351a0bbf3d378a1a45c4a28558af2608af1400
JONSKI_PC\jonch <jonchisko@gmail.com>
2023-07-28 09:28:28 +0200

    Refactor to use only one test in order to avoid binding issues

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,7 +5,7 @@ const RESPONSE_ERR: &'static str = "Failed to execute request.";
 const EXPECTED_HEALTH_CHECK_LOCATION: &'static str = "http://127.0.0.1:8000/health_check";
 
 #[tokio::test]
-async fn health_check_expect_status_200_test() {
+async fn health_check_test() {
     // Arrange
     spawn_app();
     let client = reqwest::Client::new();
@@ -19,22 +19,6 @@ async fn health_check_expect_status_200_test() {
 
     // Assert
     assert!(response.status().is_success());
-}
-
-#[tokio::test]
-async fn health_check_expect_content_length_0_test() {
-    // Arrange
-    spawn_app();
-    let client = reqwest::Client::new();
-
-    // Act
-    let response = client
-        .get(EXPECTED_HEALTH_CHECK_LOCATION)
-        .send()
-        .await
-        .expect(RESPONSE_ERR);
-
-    // Assert
     assert_eq!(Some(0), response.content_length());
 }
 
