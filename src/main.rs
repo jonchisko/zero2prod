581def4890a27c709b036d9744f946f6a08689ad
JONSKI_PC\jonch <jonchisko@gmail.com>
2023-07-31 20:53:02 +0200

    Refactor the project structure

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::startup;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    startup::run(address)?.await
 }
