2d3cdd2ff1683e52b756ab51ed42313ac8fee159
JONSKI_PC\jonch <jonchisko@gmail.com>
2023-08-02 08:09:44 +0200

    Add connection into the app state

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
 use zero2prod::{configuration::get_configuration, startup};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
-    startup::run(listener)?.await
+    startup::run(listener, connection)?.await
 }
