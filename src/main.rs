354a5a21999d5a55caa55b6a7ae2d5f33ddeeae7
JONSKI_PC\jonch <jonchisko@gmail.com>
2023-08-11 12:45:32 +0200

    Add implementation of subscription feature

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::net::TcpListener;
 
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
-    startup::run(listener, connection)?.await
+    startup::run(listener, connection_pool)?.await
 }
