fc4990f7535f03bf5e0bff787a862bfb3faa1178
JONSKI_PC\jonch <jonchisko@gmail.com>
2023-08-19 16:48:10 +0200

    Add telemetry implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use zero2prod::{configuration::get_configuration, startup};
+use zero2prod::{configuration::get_configuration, startup, telemetry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into());
+    telemetry::init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
