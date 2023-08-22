bdf5a4d8468922acd68297d6a53a73dc4edbc2cf
JONSKI_PC\jonch <jonchisko@gmail.com>
2023-08-22 12:33:20 +0200

    Add new configuration options

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Latest rust.
-FROM rust:1.63.0
+FROM rust:1.65.0
 
 # Equivalent to cd app. Docker creates the app folder if it does not exist.
 WORKDIR /app
@@ -10,8 +10,12 @@ RUN apt update && apt install lld clang -y
 # Copy all from OUR working environment to our Docker image
 COPY . .
 
+ENV SQLX_OFFLINE true
+
 # Build the binary.
 RUN cargo build --release
+
+ENV APP_ENVIRONMENT production
 
 # When docker run is executed, launch the binary.
 ENTRYPOINT [ "./target/release/zero2prod" ]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::{configuration::get_configuration, startup, telemetry};
 
 #[tokio::main]
@@ -10,11 +10,14 @@ async fn main() -> Result<(), std::io::Error> {
     telemetry::init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres.");
 
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
+    let listener = TcpListener::bind(format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    ))?;
     startup::run(listener, connection_pool)?.await
 }
